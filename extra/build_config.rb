604542889bd8533bb9800686bfe9c00b7c08b788
masada-tea <64900337+masada-tea@users.noreply.github.com>
2026-02-25 05:09:27 +0900

    build_config.rb を更新

--- extra/build_config.rb
+++ extra/build_config.rb
@@ -5,16 +5,32 @@ end
 
 MRuby::CrossBuild.new('wasm32-unknown-gnu') do |conf|
     toolchain :clang
-  
-    conf.gembox 'default'
+
+    # gembox 'default'は使わない（mruby-onig-regexpを含むため）
+    conf.gem :core => 'mruby-eval'
+    conf.gem :core => 'mruby-string-ext'
+    conf.gem :core => 'mruby-numeric-ext'
+    conf.gem :core => 'mruby-array-ext'
+    conf.gem :core => 'mruby-hash-ext'
+    conf.gem :core => 'mruby-range-ext'
+    conf.gem :core => 'mruby-enum-ext'
+    conf.gem :core => 'mruby-compar-ext'
+    conf.gem :core => 'mruby-object-ext'
+    conf.gem :core => 'mruby-kernel-ext'
+    conf.gem :core => 'mruby-print'
+    conf.gem :core => 'mruby-io'
+    conf.gem :core => 'mruby-math'
+    conf.gem :core => 'mruby-time'
+    conf.gem :core => 'mruby-struct'
+    conf.gem :core => 'mruby-compiler'
+
     conf.gem :github => 'pulsejet/mruby-marshal'
     conf.gem :github => 'monochromegane/mruby-time-strftime'
-    conf.gem :core => 'mruby-eval'
+
     conf.cc.command = 'emcc'
     conf.cc.flags = %W(-O3 -g0)
     conf.cxx.command = 'em++'
     conf.cxx.flags = %W(-O3 -g0 -std=c++14)
-  
     conf.linker.command = 'emcc'
     conf.archiver.command = 'emar'
 end
