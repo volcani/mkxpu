c07b8987db597c2dc2f8823560e71453beeb9249
masada-tea <64900337+masada-tea@users.noreply.github.com>
2026-02-25 05:12:12 +0900

    build_config.rb を更新

--- extra/build_config.rb
+++ extra/build_config.rb
@@ -28,9 +28,9 @@ MRuby::CrossBuild.new('wasm32-unknown-gnu') do |conf|
     conf.gem :github => 'monochromegane/mruby-time-strftime'
 
     conf.cc.command = 'emcc'
-    conf.cc.flags = %W(-O3 -g0)
+    conf.cc.flags = %W( -g0)
     conf.cxx.command = 'em++'
-    conf.cxx.flags = %W(-O3 -g0 -std=c++14)
+    conf.cxx.flags = %W( -g0 -std=c++14)
     conf.linker.command = 'emcc'
     conf.archiver.command = 'emar'
 end
