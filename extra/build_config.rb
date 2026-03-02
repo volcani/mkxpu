fb7f12f5e4a933fd48db0fe8f35c0c22a0a820a9
volcani <catofulthar@gmail.com>
2026-03-03 01:07:44 +0900

    build_config.rb を更新

--- extra/build_config.rb
+++ extra/build_config.rb
@@ -5,34 +5,13 @@ end
 
 MRuby::CrossBuild.new('wasm32-unknown-gnu') do |conf|
     toolchain :clang
-
-    # gemboxを使わず個別指定（mruby-onig-regexpを完全除外）
-    conf.gem :core => 'mruby-io'
-    conf.gem :core => 'mruby-math'
-    conf.gem :core => 'mruby-time'
-    conf.gem :core => 'mruby-struct'
-    conf.gem :core => 'mruby-compar-ext'
-    conf.gem :core => 'mruby-enum-ext'
-    conf.gem :core => 'mruby-string-ext'
-    conf.gem :core => 'mruby-numeric-ext'
-    conf.gem :core => 'mruby-array-ext'
-    conf.gem :core => 'mruby-hash-ext'
-    conf.gem :core => 'mruby-range-ext'
-    conf.gem :core => 'mruby-proc-ext'
-    conf.gem :core => 'mruby-symbol-ext'
-    conf.gem :core => 'mruby-random'
-    conf.gem :core => 'mruby-object-ext'
-    conf.gem :core => 'mruby-objectspace'
-    conf.gem :core => 'mruby-fiber'
-    conf.gem :core => 'mruby-enumerator'
-    conf.gem :core => 'mruby-enum-lazy'
-    conf.gem :core => 'mruby-toplevel-ext'
-    conf.gem :core => 'mruby-kernel-ext'
-    conf.gem :core => 'mruby-class-ext'
-    conf.gem :core => 'mruby-metaprog'
-    conf.gem :core => 'mruby-eval'
+  
+    conf.gembox 'default'
     conf.gem :github => 'pulsejet/mruby-marshal'
     conf.gem :github => 'monochromegane/mruby-time-strftime'
+    conf.gem :core => 'mruby-eval'
+    # mruby-onig-regexpをmruby 2.x互換の古いコミットに固定
+    conf.gem :github => 'mattn/mruby-onig-regexp', :commit => '3c1a8c4'
 
     conf.cc.command = 'emcc'
     conf.cc.flags = %W(-O3 -g0)
