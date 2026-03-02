e521dfe6e10e4b65db144e3a45f2999744d29c7a
volcani <catofulthar@gmail.com>
2026-03-03 01:19:54 +0900

    build_config.rb を更新

--- extra/build_config.rb
+++ extra/build_config.rb
@@ -5,18 +5,22 @@ end
 
 MRuby::CrossBuild.new('wasm32-unknown-gnu') do |conf|
     toolchain :clang
-  
+
     conf.gembox 'default'
+
+    # mruby-onig-regexpをmruby 2.x互換の古いコミットに固定（canonical:trueで優先）
+    # presym.hが存在しない2.1.2との互換性のため最新版を上書き
+    conf.gem :github => 'mattn/mruby-onig-regexp', :commit => '3c1a8c4', :canonical => true
+
     conf.gem :github => 'pulsejet/mruby-marshal'
     conf.gem :github => 'monochromegane/mruby-time-strftime'
     conf.gem :core => 'mruby-eval'
-    # mruby-onig-regexpをmruby 2.x互換の古いコミットに固定
-    conf.gem :github => 'mattn/mruby-onig-regexp', :commit => '3c1a8c4'
 
     conf.cc.command = 'emcc'
     conf.cc.flags = %W(-O3 -g0)
     conf.cxx.command = 'em++'
     conf.cxx.flags = %W(-O3 -g0 -std=c++14)
+
     conf.linker.command = 'emcc'
     conf.archiver.command = 'emar'
 end
